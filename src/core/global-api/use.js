98107298b4e1c424403fe2e4059e72275b2863a7
tongzhenyu <742502346@qq.com>
2022-01-05 21:31:57 +0800

    feat(read): Vue.use注册插件

--- src/core/global-api/use.js
+++ src/core/global-api/use.js
@@ -4,23 +4,28 @@ import { toArray } from '../util/index'
 
 export function initUse (Vue: GlobalAPI) {
   Vue.use = function (plugin: Function | Object) {
-    // this refer to Vue
+    // 函数内this指向Vue构造器
+
+    // 定义并获取Vue._installedPlugins
     const installedPlugins = (this._installedPlugins || (this._installedPlugins = []))
+    // 若插件已经安装过，返回Vue
     if (installedPlugins.indexOf(plugin) > -1) {
       return this
     }
 
+    // 收集参数后，调用插件
     // additional parameters
-    // 将多余的参数，传递给install方法
-    const args = toArray(arguments, 1)
-    args.unshift(this)
+    const args = toArray(arguments, 1) // 收集当前函数除第一个参数（plugin）外的其他参数
+    args.unshift(this) // 参数加上Vue构造器
     if (typeof plugin.install === 'function') {
+      // 若plugin是对象，调用plugin.install，并传入参数，函数内部this指向plugin
       plugin.install.apply(plugin, args)
     } else if (typeof plugin === 'function') {
+      // 若plugin是函数，直接调用，函数内部this指向null
       plugin.apply(null, args)
     }
 
-    // 将插件保存
+    // Vue._installedPlugins中存储该插件
     installedPlugins.push(plugin)
     return this
   }
