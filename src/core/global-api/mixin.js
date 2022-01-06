a3ebf0e6ddb95fc4d9eea3a3482c9e540661a46d
tongzhenyu <742502346@qq.com>
2022-01-06 17:50:38 +0800

    feat(read): Vue.mixin的实现

--- src/core/global-api/mixin.js
+++ src/core/global-api/mixin.js
@@ -5,6 +5,7 @@ import { mergeOptions } from '../util/index'
 // global mixin
 export function initMixin (Vue: GlobalAPI) {
   Vue.mixin = function (mixin: Object) {
+    // 将mixin中的成员拷贝到Vue.options上
     this.options = mergeOptions(this.options, mixin)
     return this
   }
